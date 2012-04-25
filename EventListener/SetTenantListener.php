047a5d0571ac9759b693694f0882194306d50054
Adrian Schneider <siradrian@gmail.com>
2012-04-25 16:35:42 -0700

    removed deps on Annotation parsing

--- EventListener/SetTenantListener.php
+++ EventListener/SetTenantListener.php
@@ -2,57 +2,35 @@
 
 namespace Synd\MultiTenantBundle\EventListener;
 
-use Synd\MultiTenantBundle\Tenant\TenantInterface;
+use Synd\MultiTenantBundle\Entity\MultiTenantInterface;
 use Doctrine\Common\EventArgs;
 use Doctrine\Common\EventSubscriber;
 
 class SetTenantListener implements EventSubscriber
 {
-    /**
-     * @var    AnnotationDrive
-     */
-    protected $driver;
-    
-    /**
-     * @var    AdapterInterface
-     */
-    protected $adpater;
-    
-    /**
-     * Creates a new instance
-     * @param    AnnotationDriver
-     * @param    AdapterInterface
-     */
-    public function __construct(AnnotationDriver $driver, AdapterInterface $adapter)
-    {
-        $this->driver = $driver;
-        $this->adapter = $adapter;
-    }
-    
     public function getSubscribedEvents()
     {
-        return array(
-            'prePersist'
-        );
+        return array('prePersist');
     }
     
     /**
-     * Check for @MultiTenant object being saved, and set the Tenant if necessary
+     * If entity being persisted is multi-tenant capable, assign the 
+     * active tenant to it and recalculate changes.
+     * 
      * @param    EventArgs
      */
     public function prePersist(EventArgs $args)
     {
-        $entity = $this->adapter->getObjectFromArs($args);
-        $config = $this->driver->readAnnotation($obj);
-        $tenant = $this->driver->readTenantAnnotation($obj);
+        $em = $args->getEntityManager();
+        $entity = $args->getEntity();
         
-        if ($tenant instanceof TenantInterface) {
+        if ($entity instanceof MultiTenantInterface and $tenant = $em->getTenant()) {
             $entity->setTenant($tenant);
+            
+            $metadata = $em->getClassMetadata(get_class($entity));
+
+            $uow = $em->getUnitOfWork();
+            $uow->recomputeSingleEntityChangeSet($metadata, $entity);
         }
-    }
-    
-    protected function updateObject($obj, TenantInterface $tenant = null, EventArgs $eventArgs = null)
-    {
-        $obj->setTenant($tenant);
     }
 }
